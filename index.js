36bfa09a12080907b0f621cd163440d39215ba50
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-10-04 17:46:54 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string){
+    let str = string
+    return str.toUpperCase()
+}
+
+function whisper(string){
+    let str = string
+    return str.toLowerCase()
+}
+
+function logShout(string){
+    let str = string
+    console.log(str.toUpperCase())
+}
+
+function logWhisper(string){
+    let str = string
+    console.log(str.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string){
+    let str = string
+
+    if (string === 'hello')
+   return"I can\'t hear you!"
+      else if (string === 'HELLO')
+    return "YES INDEED!"
+     else (string === "I love you, Grandma.")
+     return "I would love to!"
+}
+
+
